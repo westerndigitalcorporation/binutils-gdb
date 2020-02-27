067bfe20b92dd45d5fd7098277c561a6f3500158
Andrew Burgess <andrew.burgess@embecosm.com>
2020-02-27 12:46:35 +0000

    gdb: Restructure of the Python Overlay Manager
    Restructure the Python Overlay Manager code based on my improving
    understanding of how the system works.  Lays the ground work for being
    able to access the extents of the storage and cache areas even before
    ComRV has been initialised.

--- overlay-manager/ovlymgr.py
+++ overlay-manager/ovlymgr.py
@@ -1,6 +1,9 @@
 import gdb
 import re
 
+# Required to make calls to super () work in python2.
+__metaclass__ = type
+
 INIT_SYMBOL = "g_stComrvCB.ucTablesLoaded"
 OVERLAY_STORAGE_START_SYMBOL = "OVERLAY_START_OF_OVERLAYS"
 OVERLAY_STORAGE_END_SYMBOL = "OVERLAY_END_OF_OVERLAYS"
@@ -15,102 +18,130 @@ OVERLAY_MIN_CACHE_ENTRY_SIZE_IN_BYTES = 512
 # for this constant.
 # The overlay tables are loaded into the last page of this cache
 # area.
-class overlay_cache_data:
-    __instance = None
-    __mem_re = None
+class overlay_data:
+    _instance = None
+    _mem_re = None
 
     # Holds information about all the groups and multi-groups.
-    class __overlay_group_data:
+    class _overlay_group_data:
         def __init__ (self, groups, multi_groups):
-            self.__groups = groups
-            self.__multi_groups = multi_groups
+            self._groups = groups
+            self._multi_groups = multi_groups
 
         def get_group (self, index):
-            return self.__groups[index]
+            return self._groups[index]
 
     # Holds information about a single group.
-    class __overlay_group:
+    class _overlay_group:
         def __init__ (self, base_address, size_in_bytes):
-            self.__base_address = base_address
-            self.__size_in_bytes = size_in_bytes
+            self._base_address = base_address
+            self._size_in_bytes = size_in_bytes
 
         def base_address (self):
-            return self.__base_address
+            return self._base_address
 
         def size_in_bytes (self):
-            return self.__size_in_bytes
+            return self._size_in_bytes
 
-    class __cache_descriptor:
+    # A class to describe an area of memory.  This serves as a base
+    # class for the cache region descriptor, and the storage region
+    # descriptor classes.
+    class _memory_region:
+        # The START is the first address within the region, while END
+        # is the first address just beyond the region.
         def __init__ (self, start, end):
-            self.__start_address = start
-            self.__end_address = end
+            self._start_address = start
+            self._end_address = end
+
+        # Returns the first address within the region.
+        def start_address (self):
+            return self._start_address
+
+        # Return the first address past the end of the region.
+        def end_address (self):
+            return self._end_address
+
+    # A static description of the overlay cache area.  This is the
+    # area of memory into which overlays are loaded so they can be
+    # used.
+    class _cache_descriptor (_memory_region):
+        def __init__ (self, start, end):
+            super (overlay_data._cache_descriptor, self).__init__ (start, end)
 
         # Return the address for the start of the cache region.
-        def cache_base_address (self):
-            return self.__start_address
+        def base_address (self):
+            return self.start_address ()
 
         # Return the total size of the cache in bytes, including the tables
         # region.
-        def cache_size_in_bytes (self):
-            return self.__end_address - self.__start_address
+        def size_in_bytes (self):
+            return self.end_address () - self.start_address ()
 
         # Return the number of entries that are available for holding
         # overlays.  This excludes the area that is given up to hold the
         # overlay tables.  Currently the tables are copied into the last entry
         # in the cache.
-        def working_number_of_entries (self):
-            entry_size = self.cache_entry_size_in_bytes ()
-            return ((self.cache_size_in_bytes () / entry_size)
+        def number_of_working_entries (self):
+            entry_size = self.entry_size_in_bytes ()
+            return ((self.size_in_bytes () / entry_size)
                     - (self.tables_size_in_bytes () / entry_size))
 
         # Return the total number of entries that are in the cache, this
         # includes any entries being used to hold the overlay tables.
         def total_number_of_entries (self):
-            entry_size = self.cache_entry_size_in_bytes ()
+            entry_size = self.entry_size_in_bytes ()
             return (self.cache_size_in_bytes () / entry_size)
 
         # The address of the overlay tables within the cache.  Currently these
         # are always in the last entry of the cache, and are one entry in size.
         def tables_base_address (self):
-            entry_size = self.cache_entry_size_in_bytes ()
-            return self.__end_address - self.tables_size_in_bytes ()
+            entry_size = self.entry_size_in_bytes ()
+            return self.end_address () - self.tables_size_in_bytes ()
 
         # Return the size of the overlay tables region in bytes.  This is
         # currently always a single page of the cache.
         def tables_size_in_bytes (self):
-            return self.cache_entry_size_in_bytes ()
+            return self.entry_size_in_bytes ()
 
         # Return the size in bytes of a single entry (or page) within the
         # cache.
-        def cache_entry_size_in_bytes (self):
+        def entry_size_in_bytes (self):
             return OVERLAY_MIN_CACHE_ENTRY_SIZE_IN_BYTES
+
+    # A class that describes the overlay systems storage area.  This
+    # is the area of memory from which the overlays are loaded.  The
+    # debug information will refer to this area,
+    class _storage_descriptor (_memory_region):
+        def __init__ (self, start, end):
+            super (overlay_data._storage_descriptor, self).__init__ (start, end)
 
     # A wrapper class to hold all the different information we loaded from
     # target memory.  An instance of this is what we return from the fetch
     # method.
-    class __cache_data:
-        def __init__ (self, cache_descriptor, groups_data):
-            self.__cache_descriptor = cache_descriptor
-            self.__groups_data = groups_data
+    class _overlay_data_inner:
+        def __init__ (self, cache_descriptor, storage_descriptor, groups_data):
+            self._cache_descriptor = cache_descriptor
+            self._groups_data = groups_data
+            self._storage_descriptor = storage_descriptor
 
-        def working_number_of_entries (self):
-            return self.__cache_descriptor.working_number_of_entries ()
+        def cache (self):
+            return self._cache_descriptor
 
-        def cache_base_address (self):
-            return self.__cache_descriptor.cache_base_address ()
+        def storage (self):
+            return self._storage_descriptor
 
-        def get_group (self, index):
-            return self.__groups_data.get_group (index)
+        def group (self, index):
+            return self._groups_data.get_group (index)
 
-        def cache_entry_size_in_bytes (self):
-            return self.__cache_descriptor.cache_entry_size_in_bytes ()
+        def comrv_initialised (self):
+            return (not self._groups_data == None)
 
     # Read the group offset for overlay group GROUP_NUMBER.  The
     # overlay data starts at address BASE_ADDRESS in memory.
     #
     # Return the offset in bytes for the specified overlay group.
     @staticmethod
-    def __read_overlay_offset (base_address, end_address, group_number):
+    def _read_overlay_offset (base_address, end_address, group_number):
         base_address = base_address + (2 * group_number)
         if ((base_address + 1) >= end_address):
             raise RuntimeError ("out of bounds access while reading offset "
@@ -123,10 +154,10 @@ class overlay_cache_data:
         output = gdb.execute (cmd, False, True)
         output = output.split ("\n")
         output = output[-2]
-        if (overlay_cache_data.__mem_re == None):
-            overlay_cache_data.__mem_re \
+        if (overlay_data._mem_re == None):
+            overlay_data._mem_re \
                 = re.compile (r"0x[0-9a-f]+:\s+(0x[0-9a-f]+)")
-        m = overlay_cache_data.__mem_re.match (output)
+        m = overlay_data._mem_re.match (output)
         if (m == None):
             raise RuntimeError ("failed to parse memory value from %s" % output)
         scaled_offset = int (m.group (1), 16)
@@ -137,7 +168,7 @@ class overlay_cache_data:
     # overlay cache tables, and return an instance of an object holding all of
     # this data.
     @staticmethod
-    def __load_group_data (table_start, table_size, storage_start):
+    def _load_group_data (table_start, table_size, storage_start):
         groups = list ()
         multi_groups = list ()
 
@@ -148,7 +179,7 @@ class overlay_cache_data:
         grp = 0
         while (True):
             offset \
-                = overlay_cache_data.__read_overlay_offset (table_start,
+                = overlay_data._read_overlay_offset (table_start,
                                                             table_end,
                                                             grp)
 
@@ -161,80 +192,81 @@ class overlay_cache_data:
             # Calculate the size of this overlay group, and create an
             # object to represent it.
             size = offset - prev_offset
-            groups.append (overlay_cache_data.
-                           __overlay_group (storage_start + offset, size))
+            groups.append (overlay_data.
+                           _overlay_group (storage_start + offset, size))
             grp += 1
             prev_offset = offset
 
         # This is where multi-group tokens should be loaded, but this
         # is not done yet.
 
-        return (overlay_cache_data.
-                __overlay_group_data (groups, multi_groups))
+        return (overlay_data.
+                _overlay_group_data (groups, multi_groups))
 
     # Read the address of symbol NAME from the inferior, return the
     # address as an integer.
     @staticmethod
-    def __read_symbol_address_as_integer (name):
+    def _read_symbol_address_as_integer (name):
         return int (gdb.parse_and_eval ("&%s" % (name)))
 
     # Read the value of symbol NAME from the inferior, return the
     # value as an integer.
     @staticmethod
-    def __read_symbol_value_as_integer (name):
+    def _read_symbol_value_as_integer (name):
         return int (gdb.parse_and_eval ("%s" % (name)))
 
     # Load from target memory information about the overlay cache and the
     # overlay groups.
     @staticmethod
     def fetch ():
-        if (overlay_cache_data.__instance != None):
-            return overlay_cache_data.__instance
-
-        # First we must check that ComRV has been initialised.
-        init_been_called = overlay_cache_data.\
-                           __read_symbol_value_as_integer (INIT_SYMBOL)
-        if (not init_been_called):
-            return None
+        if (overlay_data._instance != None):
+            return overlay_data._instance
 
         # The overlay cache is defined by two symbols, a start and end
         # symbol.  Read these and create a cache descriptor object.
-        cache_start = overlay_cache_data.\
-                      __read_symbol_address_as_integer \
+        cache_start = overlay_data.\
+                      _read_symbol_address_as_integer \
 				(OVERLAY_CACHE_START_SYMBOL)
-        cache_end = overlay_cache_data.\
-                    __read_symbol_address_as_integer \
+        cache_end = overlay_data.\
+                    _read_symbol_address_as_integer \
 				(OVERLAY_CACHE_END_SYMBOL)
-        cache_desc = overlay_cache_data.__cache_descriptor (cache_start,
-                                                            cache_end)
+        cache_desc = overlay_data._cache_descriptor (cache_start, cache_end)
 
-        # Now load the overlay group data from the tables within the cache.
-        # In order to know where overlay groups are being loaded from we need
-        # to find their storage base address.
-        storage_start = overlay_cache_data.\
-                        __read_symbol_address_as_integer \
+        # Similarly, the storage area, where overlays are loaded from, is
+        # defined by a start and end symbol.
+        storage_start = overlay_data.\
+                        _read_symbol_address_as_integer \
 				(OVERLAY_STORAGE_START_SYMBOL)
-        storage_end = overlay_cache_data.\
-                        __read_symbol_address_as_integer \
+        storage_end = overlay_data.\
+                        _read_symbol_address_as_integer \
 				(OVERLAY_STORAGE_END_SYMBOL)
-        groups_data = overlay_cache_data.\
-                      __load_group_data (cache_desc.tables_base_address (),
-                                         cache_desc.tables_size_in_bytes (),
-                                         storage_start)
+        storage_desc \
+            = overlay_data._storage_descriptor (storage_start, storage_end)
+
+        # Finally, if ComRV has been initialised then load the current state
+        # from memory.
+        init_been_called = overlay_data.\
+                           _read_symbol_value_as_integer (INIT_SYMBOL)
+        if (init_been_called):
+            groups_data = overlay_data.\
+                          _load_group_data (cache_desc.tables_base_address (),
+                                            cache_desc.tables_size_in_bytes (),
+                                            storage_start)
+        else:
+            groups_data = None
 
         # Now package all of the components into a single class instance that
         # we return.
-
-        overlay_cache_data.__instance \
-            = overlay_cache_data.__cache_data (cache_desc, groups_data)
-
-        return overlay_cache_data.__instance
+        overlay_data._instance \
+            = overlay_data._overlay_data_inner (cache_desc, storage_desc,
+                                                 groups_data)
+        return overlay_data._instance
 
     # Discard the information loaded from the cache.  The next time fetch is
     # called the information will be reread.
     @staticmethod
     def clear ():
-        overlay_cache_data.__instance = None
+        overlay_data._instance = None
 
 # Class for walking the overlay data structures and calling the
 # visit_mapped_overlay method for every mapped overlay group.
@@ -242,19 +274,15 @@ class mapped_overlay_group_walker:
     # Call this to walk the overlay manager data structures in memory and
     # call the visit_mapped_overlay method for each mapped overlay group.
     def walk_mapped_overlays (self):
-        # Grab the data that describes the static overlay state.  This data
-        # is only loaded the first time these functions are called.
-        cache_data = overlay_cache_data.fetch ()
-
-        # If cache_data is None then this indicates that ComRV has not yet
-        # been initialised.
-        if (cache_data == None):
+        # Grab the data that describes the current overlay state.
+        ovly_data = overlay_data.fetch ()
+        if (not ovly_data.comrv_initialised ()):
             self.comrv_not_initialised ()
             return
 
         # Now walk the overlay cache and see which entries are mapped in.
         index = 0
-        while (index < cache_data.working_number_of_entries ()):
+        while (index < ovly_data.cache ().number_of_working_entries ()):
 
             group = gdb.parse_and_eval ("g_stComrvCB.stOverlayCache[%d].unToken.stFields.uiOverlayGroupID" % (index))
             group = int (group)
@@ -263,12 +291,12 @@ class mapped_overlay_group_walker:
             if (group != 0xffff):
                 # Found an entry that is mapped in.
 
-                group_desc = cache_data.get_group (group)
+                group_desc = ovly_data.group (group)
                 src_addr = group_desc.base_address ()
                 length = group_desc.size_in_bytes ()
-                dst_addr = (cache_data.cache_base_address ()
+                dst_addr = (ovly_data.cache ().base_address ()
                             + (index
-                               * cache_data.cache_entry_size_in_bytes ()))
+                               * ovly_data.cache ().entry_size_in_bytes ()))
 
                 if (not self.visit_mapped_overlay (src_addr, dst_addr, length,
                                                 index, group)):
@@ -327,11 +355,19 @@ class ParseComRV (gdb.Command):
         # Discard the cached cache data, incase we ran this command at the
         # wrong time and the cache information is invalid.  This will force
         # GDB to reload the information each time this command is run.
-        overlay_cache_data.clear ()
+        overlay_data.clear ()
 
 class MyOverlayManager (gdb.OverlayManager):
     def __init__ (self):
         gdb.OverlayManager.__init__ (self, True)
+        ovly_data = overlay_data.fetch ()
+        print ("Setting up Overlay Manager:")
+        print ("Cache:")
+        print ("  Start: 0x%x" % (ovly_data.cache ().start_address ()))
+        print ("    End: 0x%x" % (ovly_data.cache ().end_address ()))
+        print ("Storage:")
+        print ("  Start: 0x%x" % (ovly_data.storage ().start_address ()))
+        print ("    End: 0x%x" % (ovly_data.storage ().end_address ()))
 
     def __del__ (self):
         print ('Destructor called for MyOverlayManager')
@@ -363,12 +399,12 @@ class MyOverlayManager (gdb.OverlayManager):
         # mapped overlays.
         class map_overlays (mapped_overlay_group_walker):
             def __init__ (self, manager):
-                self.__manager = manager
+                self._manager = manager
                 self.walk_mapped_overlays ()
 
             def visit_mapped_overlay (self, src_addr, dst_addr, length,
                                       cache_index, group_number):
-                self.__manager.add_mapping (src_addr, dst_addr, length)
+                self._manager.add_mapping (src_addr, dst_addr, length)
                 return True
 
         # Create an instance of the previous class, this does all the work in
